f887d896f9d7a6b3edba4e8b01ef16eb604b1e25
Psychrode <97118606+Psychrode@users.noreply.github.com>
2023-04-16 20:33:07 -0400

    Update LinkedStackTest.java

--- LinkedStackTest.java
+++ LinkedStackTest.java
@@ -1,8 +1,4 @@
 package towerofhanoi;
-// Virginia Tech Honor Code Pledge:
-// As a Hokie, I will conduct myself with honor and integrity at all times.
-// I will not lie, cheat, or steal, nor will I accept the actions of those who
-// do.
 // -- Solaiman Ibrahimi (kuplar)
 
 import java.util.EmptyStackException;
